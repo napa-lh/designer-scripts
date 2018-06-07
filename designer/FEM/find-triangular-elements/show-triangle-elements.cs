07fdab0ee02cc0a55c20887cf70f70187c824826
Jan Mattsson <>
2018-06-07 08:59:20 +0300

    example to illustrate the concept

---
+++ designer/FEM/find-triangular-elements/show-triangle-elements.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel;
+using System.Linq;
+using Napa.Gui;
+using Napa.Core.Steel;
+using Napa.Scripting;
+
+/// <summary>
+///    <para>
+///       This script will color triangular FEM elements red so that they are 
+///       easy to find and fix the mesh in those locations.
+///    </para>
+///    <para>
+///       After installing the script, you will have a new command
+///       SHOW_TRIANGULAR_ELEMENTS, which will activate this script. 
+///       The command is available when you are in the modeling view.
+///    </para>
+/// </summary>
+/// <remarks>
+///    This is a placeholder for testing the script distribution mechanism.
+/// </remarks>
+/// 
+
+public class ShowTringularElements : ScriptBase {
+
+    
+    public override void Run() {    
+        // ...
+    }
+    
+  
+}
+    
